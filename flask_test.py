20b7bcd44b62da551bb593e71f28787b31e82bb1
poulamipaul <56878269+poulamipaul@users.noreply.github.com>
2020-04-24 13:12:32 +0530

    Add files via upload

---
+++ flask_test.py
@@ -0,0 +1,84 @@
+import base64
+import numpy as np
+import io
+from PIL import Image
+import cv2
+import keras_ocr
+
+import matplotlib.pyplot as plt
+import keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array
+from flask import request
+from flask import jsonify
+from flask import Flask
+#from flask_cors import CORS, cross_origin
+from flask import render_template
+
+app=Flask(__name__)
+
+#cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:5000"}})
+#app.config['CORS_HEADERS'] = 'Content-Type'
+
+#@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+
+@app.route("/")
+def startWeb():
+    return render_template('hello.html')
+
+@app.route("/hello",methods=["GET","POST"])
+def hello():
+    
+    message=request.get_json(force=True)
+    encoded=message['image']
+    decoded=base64.b64decode(encoded)
+    
+    image=Image.open(io.BytesIO(decoded))
+   
+    img=image.save("ocr.jpg"); 
+    print(img)
+    img=keras_ocr.tools.read("C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python36\\ocr.jpg")
+    pipeline = keras_ocr.pipeline.Pipeline()
+    predictions = pipeline.recognize(images=[img])[0]
+    #drawn = keras_ocr.tools.drawBoxes(image=img, boxes=predictions, boxes_format='predictions')
+    x= ([text for text, box in predictions])
+    index=0
+    pred=[]
+    for i in range(0,6):
+      if(x[i].isalpha() and len(x[i])==4):
+        pred.append(x[i].upper())
+        index=i
+  
+    for i in range(0,6):
+      if(len(x[i])>=5 and x[i].isnumeric()):
+        pred.append(x[i])
+
+    for i in range(0,6):
+      if(len(x[i])==1):
+        pred.append(x[i].upper())
+    for i in range(0,6):
+      if(len(x[i])==4 and i!=index):
+        pred.append(x[i].upper())
+        
+    
+    
+    
+
+
+    ans=pred[0]+pred[1]+pred[2]+pred[3]
+    
+    print(ans)
+    
+                    
+
+    response={'prediction':ans}
+    
+    return jsonify(response)
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+    
